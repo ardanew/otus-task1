72f02de4f23f419f0f87ae542de8ec314b8e75f8
Ivlev A.S <ivlevas@ircoc.vrn.ru>
2024-11-02 12:00:43 +0300

    add: test PATCH_NUM value

--- tests/test_hello.cpp
+++ tests/test_hello.cpp
@@ -1,11 +1,11 @@
 #include <sstream>
-
 #include <gtest/gtest.h>
-
-#include <hello.h>
+#include "hello.h"
+#include "version.h"
 
 TEST(Hello, simple) {
   std::stringstream ss;
   hello::greetings(ss);
-  ASSERT_NE(std::string::npos, ss.str().find("Hello, world!"));
+  EXPECT_NE(std::string::npos, ss.str().find("Hello, world!"));
+  EXPECT_TRUE(std::atoi(PATCH_NUM) > 0);
 }
